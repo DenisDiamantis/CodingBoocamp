345ad024d17ad6914987bd1427b826e796b91da7
Denis Diamantis <denisdiam@hotmail.com>
2023-11-09 17:28:58 +0200

    Add files via upload

--- Bootcamp/Vue/modal-project/src/App.vue
+++ Bootcamp/Vue/modal-project/src/App.vue
@@ -1,15 +1,79 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <p>Welcome</p>
+  <!-- <input type="text" ref="name">
+  <button @click="handleClick">Click me</button>  -->
+  <!-- Teleport -->
+  <!-- <teleport to=".modals" v-if="showModal">
+    <ModalV theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">Sign up now</a>
+
+        <a href="#">More info</a>
+      </template>
+      <h1>Correct Giveaway!</h1>
+      <p>Grab your training for HALVE price!</p>
+    </ModalV>
+  </teleport> -->
+  <div v-if="showModal">
+    <ModalV theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">Sign up now</a>
+
+        <a href="#">More info</a>
+      </template>
+      <h1>Correct Giveaway!</h1>
+      <p>Grab your training for HALVE price!</p>
+    </ModalV>
+  </div>
+  <div v-if="showModal2">
+    <ModalV theme="greed" @close="toggleModal2">
+      <template v-slot:links2>
+        <a href="#">Sign up now</a>
+
+        <a href="#">More info</a>
+      </template>
+      <h1>Wrong Giveaway!</h1>
+      <p>Grab your training for DOUBLE price!</p>
+    </ModalV>
+  </div>
+
+  <button @click="toggleModal">Open Modal</button>
+  <button @click="toggleModal2">Open Modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ModalV from './components/ModalV.vue'
+
+
 
 export default {
   name: 'App',
+  data() {
+    return {
+      title: 'My First Vue App',
+      // header: 'Sign up for the Giveaway!',
+      // text: 'Grab your training for halve price!',
+      showModal: false,
+      showModal2: false
+
+    }
+  },
+  methods: {
+    // handleClick() {
+    //   console.log(this.$refs.name)
+    //   this.$refs.name.classList.add('active')
+    //   this.$refs.name.focus()
+    // }
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModal2() {
+      this.showModal2 = !this.showModal2;
+    }
+  },
   components: {
-    HelloWorld
+    ModalV
   }
 }
 </script>
@@ -22,5 +86,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
---
+++ Bootcamp/Vue/modal-project/src/components/ModalV.vue
@@ -0,0 +1,103 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="theme">
+            <!-- <h1>{{ header }}</h1>
+            <p>{{ text }}</p> -->
+            <slot></slot>
+            <div class="actions2">
+                <slot name="links2"></slot>
+            </div>
+            <div class="actions">
+                <slot name="links"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['theme'],
+    methods: {
+        closeModal() {
+            this.$emit('close')
+        }
+    }
+}
+
+</script>
+
+<style>
+.modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+}
+
+.backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.5);
+    width: 100%;
+    height: 100%;
+}
+
+.modal h1 {
+    color: #03cfb4;
+    border: none;
+    padding: 0;
+}
+
+.modal p {
+    font-style: normal;
+}
+
+.modal.sale {
+    background: green;
+    color: white;
+}
+
+.modal.sale h1 {
+    color: white;
+}
+
+.modal.greed {
+    background: crimson;
+    color: white;
+}
+
+.modal.greed h1 {
+    color: white;
+}
+
+.modal .actions {
+    text-align: center;
+    margin: 30px 0 10px 0;
+    color: #333;
+}
+
+.modal .actions a {
+    color: #333;
+    padding: 8px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 10px;
+}
+
+.modal .actions2 {
+    text-align: center;
+    margin: 30px 0 10px 0;
+    color: #333;
+}
+
+.modal .actions2 a {
+    color: #333;
+    padding: 8px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 10px;
+}
+</style>
