1206c5983b19d19b3cc67130f5aef988c6386004
Denis Diamantis <denisdiam@hotmail.com>
2023-11-17 17:12:46 +0200

    Add files via upload

---
+++ Bootcamp/CSharp/Fundumentals/1_FirstCSharp/Program.cs
@@ -0,0 +1,10 @@
+﻿class Sample
+{
+    static void Main()
+    {
+        //This is my first C# program
+        //ReadKey() stops error message
+        System.Console.WriteLine("Hello this is my first C# program");
+        System.Console.ReadKey();
+    }
+}
