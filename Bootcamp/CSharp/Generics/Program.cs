e1af43751804e52b16e56e16de5f1f5424a5f4db
Denis Diamantis <denisdiam@hotmail.com>
2023-11-28 17:12:32 +0200

    Add files via upload

---
+++ Bootcamp/CSharp/Generics/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main()
+    {
+        User<int, int> user1 = new User<int, int>();
+        User<bool, int> user2 = new User<bool, int>();
+        User<string, int> user3 = new User<string, int>();
+
+        user1.RegistrationStatus = 1234;
+        user1.Age = 30;
+        user2.RegistrationStatus = true;
+        user3.RegistrationStatus = "pending";
+        Console.WriteLine(user1.RegistrationStatus + " " + user1.Age);
+        Console.WriteLine(user2.RegistrationStatus);
+        Console.WriteLine(user3.RegistrationStatus);
+    }
+}
